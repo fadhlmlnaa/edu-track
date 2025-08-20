e01118df16d8ad5c4283f657185cdf8c9bf450fd
dyylms <fadhlmlnaa@gmail.com>
2025-08-20 17:00:33 +0700

    fix: change a on 404 page to link

--- app/not-found.js
+++ app/not-found.js
@@ -1,14 +1,16 @@
+import Link from "next/link";
+
 export default function NotFound() {
     return (
       <div className="flex h-screen flex-col items-center justify-center">
         <h1 className="text-5xl font-bold text-cyan-700">404</h1>
         <p className="mt-2 text-gray-600">Halaman tidak ditemukan</p>
-        <a
+        <Link
           href="/"
           className="mt-4 rounded-lg bg-cyan-600 px-4 py-2 text-white hover:bg-cyan-700"
         >
           Kembali ke Home
-        </a>
+        </Link>
       </div>
     );
   }
